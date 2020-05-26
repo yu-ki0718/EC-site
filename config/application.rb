a9a6a855b0987b659407c178f20afab5d13bdb74
岸波　雄樹 <you_work_24@icloud.com>
2020-05-26 17:14:21 +0900

    added config.i18n.load_path

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module EcSite
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Load application's model / class decorators
     initializer 'spree.decorators' do |app|
